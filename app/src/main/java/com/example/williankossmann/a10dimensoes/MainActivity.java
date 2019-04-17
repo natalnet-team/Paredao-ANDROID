2c3902e73ccf98e7b5939823562eaf1a30fb87f3
Aryclenio <aryclenio2010@gmail.com>
2019-04-17 13:17:07 -0300

    Add new functions

--- app/src/main/java/com/example/williankossmann/a10dimensoes/MainActivity.java
+++ app/src/main/java/com/example/williankossmann/a10dimensoes/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.williankossmann.a10dimensoes;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,8 +17,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -26,34 +38,73 @@ public class MainActivity extends AppCompatActivity {
     boolean conectado = false;
 
     private TextView tv_Central;
+    private TextView nuvem;
     private EditText et_ip, et_porta;
+    static String MQTTHOST = "tcp://m16.cloudmqtt.com:12046";
+    static String USERNAME = "znyeyfdl";
+    static String PASSWORD = "ufFO-2eQarFz";
+    String topicStr = "esp/test";
+    MqttAndroidClient client;
     double moduloCru;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        nuvem = findViewById(R.id.nuvem);
         tv_Central = findViewById(R.id.tv_Central);
         et_ip = findViewById(R.id.et_ip);
         et_porta = findViewById(R.id.et_porta);
-        final TextView tx = findViewById(R.id.tX);
-        final TextView ty = findViewById(R.id.tY);
-        final TextView tz = findViewById(R.id.tZ);
         final EditText msg = findViewById(R.id.field_msg);
         final Button btEnviar = findViewById(R.id.bt_msg);
         final Button btLigar = findViewById(R.id.btLigar);
         final Button btDesligar = findViewById(R.id.btDesligar);
 
         final Button button = findViewById(R.id.bt_conectar);
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), MQTTHOST, clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+        tv_Central.setTextColor(Color.RED);
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+            try {
+                IMqttToken token = client.connect(options);
+                token.setActionCallback(new IMqttActionListener() {
+                    @Override
+                    public void onSuccess(IMqttToken asyncActionToken) {
+                        Toast.makeText(MainActivity.this, "Conectado a nuvem! :) ", Toast.LENGTH_LONG).show();
+                        nuvem.setText("Conectado");
+                        nuvem.setTextColor(Color.GREEN);
+                    }
+
+                    @Override
+                    public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                        Toast.makeText(MainActivity.this, "Falha na conexao com a nuvem! :( ", Toast.LENGTH_LONG).show();
+                    }
+                });
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }else
+            connected = false;
+            nuvem.setText("Desconectado");
+            nuvem.setTextColor(Color.RED);
         button.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             public void onClick(View v) {
                 if(conectado) {
                     cliente.encerrar();
                     conectado = false;
-                    button.setText("Conectar e Enviar");
+                    button.setText("Conectar");
                     tv_Central.setText("Desconectado");
+                    tv_Central.setTextColor(Color.RED);
                 }else{
                     try {
                         String x = String.valueOf(et_ip.getText());
@@ -65,11 +116,13 @@ public class MainActivity extends AppCompatActivity {
                         Thread.sleep(500);
                         conectado = true;
                         button.setText("Desconectar");
-                        tv_Central.setText("Conectado e Enviando");
+                        tv_Central.setText("Conectado");
+                        tv_Central.setTextColor(Color.GREEN);
                     }catch (Exception e){
                         conectado = false;
-                        button.setText("Conectar e Enviar");
+                        button.setText("Conectar");
                         tv_Central.setText("Desconectado");
+                        tv_Central.setTextColor(Color.RED);
                     }
                 }
             }
@@ -77,7 +130,14 @@ public class MainActivity extends AppCompatActivity {
 
         btEnviar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                String topic = topicStr;
+                String mes = msg.getText().toString();
+                byte[] enc = new byte[0];
                 try {
+                    enc = mes.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(enc);
+                    message.setRetained(true);
+                    client.publish(topic, message);
                     String txt = msg.getText().toString();
                     cliente.sendMsg(txt);
                 } catch (Exception e) {
@@ -120,10 +180,6 @@ public class MainActivity extends AppCompatActivity {
                 float z = (event.values[2]/event.sensor.getMaximumRange()/2) * 50;
                 event.sensor.getMaximumRange();
                 moduloCru = Math.sqrt(x*x + y*y + z*z);
-
-                tx.setText("X: " + (event.values[0]));
-                ty.setText("Y: " + (event.values[1]));
-                tz.setText("Z: " + (event.values[2]));
 
                 if(moduloCru > 50){
                     moduloCru = 50;
